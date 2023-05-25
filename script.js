39efb9853672cbbd2402aa574e224b5408bb03ef
Sunwoo <csw6648@naver.com>
2023-05-25 15:48:58 +0900

    css

---
+++ script.js
@@ -0,0 +1,13 @@
+var prevScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").classList.remove("hidden");
+  } else {
+    document.getElementById("navbar").classList.add("hidden");
+  }
+
+  prevScrollPos = currentScrollPos;
+};
